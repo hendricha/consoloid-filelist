5542f8728c4b5822bc8cc72f83ebc01d744e69a1
Hendrich Attila <hendricha@agmen.hu>
2014-04-11 11:36:51 +0200

    PathValidator is now PathAbsolutifier
    It now generates absolute paths from strings if possible.

---
+++ Consoloid/FileList/PathAbsolutifier.js
@@ -0,0 +1,50 @@
+defineClass('Consoloid.FileList.PathAbsolutifier', 'Consoloid.Base.Object',
+  {
+    absolutifyFile: function(file)
+    {
+      if (this.__isAbsolutePath(file)) {
+        return file;
+      }
+
+      if (this.__notDeepRelativePath(file) && this.__getLastFileList().hasFile(file)) {
+        return this.__getLastFileList().getPath() + "/" + file;
+      }
+
+      throw new Error(__("File is either not referenced by absolute path, or not in last shown file list view."));
+    },
+
+    __isAbsolutePath: function(path)
+    {
+      return (path[0] == "/") ? true : false;
+    },
+
+    __notDeepRelativePath: function(path)
+    {
+      return (path.indexOf("/") == -1) ? true : false;
+    },
+
+    __getLastFileList: function()
+    {
+      var contextObjects = this.get("context").findByClass('Consoloid.FileList.Context.FileListDialog');
+
+      if (contextObjects.length) {
+       return contextObjects[0].entity.getList();
+      }
+
+      throw new Error(__("No file list view to find this item, try absolute path."));
+    },
+
+    absolutifyFolder: function(folder)
+    {
+      if (this.__isAbsolutePath(folder)) {
+        return folder;
+      }
+
+      if (this.__notDeepRelativePath(folder) && this.__getLastFileList().hasFolder(folder)) {
+        return this.__getLastFileList().getPath() + "/" + folder;
+      }
+
+      throw new Error(__("Folder is either not referenced by absolute path, or not in last shown file list view."));
+    },
+  }
+);
---
+++ Consoloid/FileList/test/PathAbsolutifierTest.js
@@ -0,0 +1,72 @@
+require("../PathAbsolutifier");
+require('consoloid-framework/Consoloid/Test/UnitTest');
+
+describeUnitTest('Consoloid.FileList.PathAbsolutifier', function() {
+  var
+    validator,
+    context;
+
+  beforeEach(function() {
+    context = {
+      findByClass: sinon.stub().returns([{
+        entity: {
+          getList: sinon.stub().returns({
+            hasFile: sinon.stub().returns(true),
+            hasFolder: sinon.stub().returns(true),
+            getPath: sinon.stub().returns("/something/something")
+          })
+        }
+      }])
+    };
+    env.addServiceMock("context", context);
+    validator = env.create("Consoloid.FileList.PathAbsolutifier", {});
+    global.__ = function(str) { return str; };
+  });
+
+  describe("#absolutifyFile(file)", function() {
+    it("should return with the same absolute path it was called with", function() {
+      validator.absolutifyFile("/etc/passwd").should.equal("/etc/passwd");
+    });
+
+    it("should not accept more than one deep relative paths (eg. 'Images/Something.JPG')", function() {
+      (function() {
+        validator.absolutifyFile("Images/Something.JPG");
+      }).should.throwError();
+    });
+
+    it("should accept one deep relative paths if last file list dialog has that item", function() {
+      validator.absolutifyFile("foobar").should.equal("/something/something/foobar");
+      context.findByClass.alwaysCalledWith('Consoloid.FileList.Context.FileListDialog');
+
+      context.findByClass()[0].entity.getList().hasFile.returns(false);
+
+      (function() {
+        validator.absolutifyFile("foobar");
+      }).should.throwError();
+    });
+  });
+
+  describe("#absolutifyFolder(folder)", function() {
+    it("should do everything validateFile does", function() {
+      validator.absolutifyFolder("/etc/passwd").should.equal("/etc/passwd");
+      validator.absolutifyFolder("foobar").should.equal("/something/something/foobar");
+      context.findByClass.alwaysCalledWith('Consoloid.FileList.Context.FileListDialog');
+
+      (function() {
+        validator.absolutifyFolder("Images/Something.JPG");
+      }).should.throwError();
+    });
+
+    it("should accept only folders", function() {
+      context.findByClass()[0].entity.getList().hasFolder.returns(false);
+
+      (function() {
+        validator.absolutifyFolder("foobar");
+      }).should.throwError();
+    });
+  });
+
+  afterEach(function() {
+    delete global.__;
+  })
+});
