a97bf2a9a322d86af10af60669b1816587af7832
hendricha <hendricha@gmail.com>
2014-11-25 21:25:08 +0100

    OpenerRegistration: ListItemFactory: Indented everything to match usual consoloid class indentation
    (No other change in file)

--- Consoloid/FileList/ListItemFactory.js
+++ Consoloid/FileList/ListItemFactory.js
@@ -1,44 +1,44 @@
 defineClass('Consoloid.FileList.ListItemFactory', 'Consoloid.Ui.List.Factory.Collapsing',
-{
-  __constructor: function(options)
   {
-    this.__base(options);
+    __constructor: function(options)
+    {
+      this.__base(options);
 
-    this.registrations = this.container.getAllTagged(["file-opener-registration"]);
-  },
+      this.registrations = this.container.getAllTagged(["file-opener-registration"]);
+    },
 
-  render: function(data)
-  {
-    data.additionalActions = this.__getAdditionalActions(data);
+    render: function(data)
+    {
+      data.additionalActions = this.__getAdditionalActions(data);
 
-    return this.__base(data);
-  },
+      return this.__base(data);
+    },
 
-  __getAdditionalActions: function(data)
-  {
-    if (!data.isFile) {
-      return [];
+    __getAdditionalActions: function(data)
+    {
+      if (!data.isFile) {
+        return [];
+      }
+
+      var additionalActions = [];
+
+      this.registrations.forEach(function(registration) {
+        var extensions = registration.getFileExtensions();
+        for (var i = 0; i < extensions.length; i++) {
+          if (data.name.indexOf("." + extensions[i]) == data.name.length - extensions[i].length - 1) {
+            var expression = registration.getExpression();
+            $.each(expression[1], function(key, value) {
+              if (value == "@target") {
+                expression[1][key] = data.name;
+              }
+            });
+            additionalActions.push(expression);
+            break;
+          }
+        };
+      });
+
+      return additionalActions;
     }
-
-    var additionalActions = [];
-
-    this.registrations.forEach(function(registration) {
-      var extensions = registration.getFileExtensions();
-      for (var i = 0; i < extensions.length; i++) {
-        if (data.name.indexOf("." + extensions[i]) == data.name.length - extensions[i].length - 1) {
-          var expression = registration.getExpression();
-          $.each(expression[1], function(key, value) {
-            if (value == "@target") {
-              expression[1][key] = data.name;
-            }
-          });
-          additionalActions.push(expression);
-          break;
-        }
-      };
-    });
-
-    return additionalActions;
   }
-}
 );
