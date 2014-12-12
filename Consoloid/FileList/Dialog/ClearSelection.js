36f056d62961e4ae31871cbc46f01d22799f9dc1
hendricha <hendricha@gmail.com>
2014-12-12 16:38:15 +0100

    Fix clear selection dialog
    it now starts on button click, has message

--- Consoloid/FileList/Dialog/ClearSelection.js
+++ Consoloid/FileList/Dialog/ClearSelection.js
@@ -1,5 +1,11 @@
 defineClass('Consoloid.FileList.Dialog.ClearSelection', 'Consoloid.Ui.Volatile.Dialog',
   {
+    __constructor: function(options)
+    {
+      this.__base($.extend({
+        message: "Selection cleared",
+      }, options));
+    },
 
     setup: function()
     {
